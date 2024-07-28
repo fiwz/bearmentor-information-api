4fb9248901b4bf28ba005bb5d2ebcd6d59f685c2
fiwz <afianiii@gmail.com>
2024-07-28 12:31:25 +0700

    feat: update index js to show json

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-// Create HTTP server
-const server = http.createServer(function (req, res) {
-  // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, { "Content-Type": "text/plain" });
+function handler(req, res, next) {
+  // res.writeHead(200, { "Content-Type": "text/plain" }); // Set the response HTTP header with HTTP status and Content type
+  // res.end("Hello World\n"); // Send the response body "Hello World"
 
-  // Send the response body "Hello World"
-  res.end("Hello World\n");
-});
+  // json version
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      name: "afiani",
+      batch: 3,
+    })
+  );
+}
+// Create HTTP server
+const server = http.createServer(handler);
 
 // Prints a log once the server starts listening
 server.listen(port, hostname, function () {
