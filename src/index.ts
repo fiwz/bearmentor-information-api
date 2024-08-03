81ff19e6315cd652757cd9b2f1b9936891928103
fiwz <afianiii@gmail.com>
2024-08-03 09:20:40 +0700

    feat: explore hono docs

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,32 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.json({
+    ok: true,
+    message: 'Hello Hono!',
+  })
 })
+
+app.get('/posts/:id', (c) => {
+  const page = c.req.query('page')
+  const id = c.req.param('id')
+  c.header('X-Message', 'Hi!')
+  // return c.text(`You want see ${page} of ${id}`)
+  return c.json({
+    message: 'Testing routes',
+    query: c.req.query(),
+    param: c.req.param(),
+  })
+})
+
+app.post('/posts', (c) => c.text('Created!', 201))
+
+app.delete('/posts/:id', (c) => c.text(`${c.req.param('id')} is deleted!`))
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
 
 serve({
   fetch: app.fetch,
-  port
+  port,
 })
